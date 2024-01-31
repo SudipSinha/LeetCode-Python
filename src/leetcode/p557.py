a573db5a6f38f22ec93a3ed9ccf0207fab7f1e0b
Sudip Sinha <sinhsudi@amazon.com>
2024-01-30 19:32:35 -0800

    Add P557

---
+++ src/leetcode/p557.py
@@ -0,0 +1,10 @@
+"""Reverse Words in a String III
+
+Given a string `s`, reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+"""
+
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        words = s.split(" ")
+        return " ".join(word[::-1] for word in words)
---
+++ tests/test_p557.py
@@ -0,0 +1,14 @@
+import pytest
+from leetcode import p557
+
+
+examples = [
+    ("Let's take LeetCode contest", "s'teL ekat edoCteeL tsetnoc"),
+    ("Mr Ding", "rM gniD"),
+]
+
+
+@pytest.mark.parametrize("s, output_true", examples)
+def test_reverseWords(s: str, output_true: str):
+    output_calc = p557.Solution().reverseWords(s)
+    assert output_calc == output_true
