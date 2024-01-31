9820a280736b3a527b16a6436b38b5a9b94557cc
Sudip Sinha <sinhsudi@amazon.com>
2024-01-30 19:31:59 -0800

    Add P004

---
+++ src/leetcode/p004.py
@@ -0,0 +1,17 @@
+"""Median of Two Sorted Arrays
+
+Given two sorted arrays `nums1` and `nums2` of size `m` and `n` respectively, return the median of the two sorted arrays.
+
+The overall run time complexity should be `O(log (m+n))`.
+"""
+
+
+class Solution:
+    def findMedianSortedArrays_naive(self, nums1: list[int], nums2: list[int]) -> float:
+        """Time complexity: O((m + n) log(m + n)), Space complexity: O(m + n)."""
+        merged = sorted(nums1 + nums2)
+        n = len(merged)
+        if n % 2 == 1:
+            return merged[(n - 1) // 2]
+        else:
+            return (merged[n // 2 - 1] + merged[n // 2]) / 2
---
+++ tests/test_p004.py
@@ -0,0 +1,16 @@
+import pytest
+from leetcode import p004
+
+
+examples = [
+    ([1, 3], [2], 2.0),
+    ([1, 2], [3, 4], 2.5),
+]
+
+
+@pytest.mark.parametrize("nums1, nums2, output_true", examples)
+def test_findMedianSortedArrays_naive(
+    nums1: list[int], nums2: list[int], output_true: float
+):
+    output_calc = p004.Solution().findMedianSortedArrays_naive(nums1, nums2)
+    assert output_calc == output_true
