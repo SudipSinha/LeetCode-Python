7d29aac06e009a4dcb37c2e8b3a5dff8a6e6befe
Sudip Sinha <sinhsudi@amazon.com>
2024-02-08 15:15:22 -0800

    Add DP solution for LC0392

--- src/leetcode/lc0392.py
+++ src/leetcode/lc0392.py
@@ -7,13 +7,31 @@ A subsequence of a string is a new string that is formed from the original strin
 
 
 class Solution:
-    def isSubsequence(self, s: str, t: str) -> bool:
+    def isSubsequence_simple(self, s: str, t: str) -> bool:
         """Time complexity: O(mn), Space complexity: O(1)."""
-        idx = 0
+        idx_t = 0
         for char in s:
-            while idx < len(t) and char != t[idx]:
-                idx += 1
-            idx += 1
-            if idx > len(t):
+            while idx_t < len(t) and char != t[idx_t]:
+                idx_t += 1
+            idx_t += 1
+            if idx_t > len(t):
+                return False
+        return True
+
+    def isSubsequence_dp(self, s: str, t: str) -> bool:
+        """Dynamic programming solution. Copied from:
+        https://leetcode.com/problems/is-subsequence/solutions/4074367/93-76-two-pointers-dp/
+        Time complexity: O(mn), Space complexity: O(1).
+        """
+        nxt = [{} for _ in range(len(t) + 1)]
+        for i in range(len(t) - 1, -1, -1):
+            nxt[i] = nxt[i + 1].copy()
+            nxt[i][t[i]] = i + 1
+        
+        i = 0
+        for c in s:
+            if c in nxt[i]:
+                i = nxt[i][c]
+            else:
                 return False
         return True
--- tests/tests_leetcode/test_lc0392.py
+++ tests/tests_leetcode/test_lc0392.py
@@ -8,6 +8,11 @@ examples = [
 
 
 @pytest.mark.parametrize("s, t, output_true", examples)
-def test_isSubsequence(s: str, t: str, output_true: bool):
-    output_calc = lc0392.Solution().isSubsequence(s=s, t=t)
+def test_isSubsequence_simple(s: str, t: str, output_true: bool):
+    output_calc = lc0392.Solution().isSubsequence_simple(s=s, t=t)
+    assert output_calc == output_true
+
+@pytest.mark.parametrize("s, t, output_true", examples)
+def test_isSubsequence_dp(s: str, t: str, output_true: bool):
+    output_calc = lc0392.Solution().isSubsequence_dp(s=s, t=t)
     assert output_calc == output_true
