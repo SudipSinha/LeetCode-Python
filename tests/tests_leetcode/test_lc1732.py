d079f31d57c1d126e9de5cfc343bc28fc6ba7970
Sudip Sinha <sinhsudi@amazon.com>
2024-02-10 21:28:02 -0800

    Add LC1732

---
+++ tests/tests_leetcode/test_lc1732.py
@@ -0,0 +1,13 @@
+import pytest
+from leetcode import lc1732
+
+examples = [
+    ([-5, 1, 5, 0, -7], 1),
+    ([-4, -3, -2, -1, 4, 3, 2], 0),
+]
+
+
+@pytest.mark.parametrize("gain, output_true", examples)
+def test_maxArea_naive(gain: list[int], output_true: int):
+    output_calc = lc1732.Solution().largestAltitude(gain=gain)
+    assert output_calc == output_true
