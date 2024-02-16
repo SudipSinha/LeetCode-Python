04ad186973e52d2aba41b487d4a69f296d7a9b7f
Sudip Sinha <sinhsudi@amazon.com>
2024-02-15 23:58:24 -0800

    Add LC0069

---
+++ src/leetcode/lc0069.py
@@ -0,0 +1,34 @@
+"""Sqrt(x)
+
+Link: https://leetcode.com/problems/sqrtx/description/
+
+Given a non-negative integer `x`, return the square root of `x` rounded down to the nearest integer. The returned integer should be non-negative as well.
+
+You must not use any built-in exponent function or operator.
+
+For example, do not use `pow(x, 0.5)` in C++ or `x ** 0.5` in python.
+"""
+
+
+class Solution:
+    def mySqrt_naive(self, x: int) -> int:
+        sqrt = 0
+        while sqrt * sqrt <= x:
+            sqrt += 1
+        return sqrt - 1
+
+    def mySqrt_binarysearch(self, x: int) -> int:
+        if x == 0 or x == 1:
+            return x
+        left = 0
+        right = x // 2
+        while left <= right:
+            mid = left + (right - left) // 2
+            sq = mid * mid
+            if sq == x:
+                return mid
+            elif sq < x:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return right
---
+++ tests/tests_leetcode/test_lc0069.py
@@ -0,0 +1,22 @@
+import pytest
+from leetcode import lc0069
+
+
+examples = [
+    (0, 0),
+    (1, 1),
+    (4, 2),
+    (8, 2),
+]
+
+
+@pytest.mark.parametrize("x, output_true", examples)
+def test_mySqrt_naive(x: int, output_true: int):
+    output_calc = lc0069.Solution().mySqrt_naive(x=x)
+    assert output_calc == output_true
+
+
+@pytest.mark.parametrize("x, output_true", examples)
+def test_mySqrt_binarysearch(x: int, output_true: int):
+    output_calc = lc0069.Solution().mySqrt_binarysearch(x=x)
+    assert output_calc == output_true
