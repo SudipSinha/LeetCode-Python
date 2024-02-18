6ec8205faedafcd4dae93b69684c9ed045b3b985
Sudip Sinha <sinhsudi@amazon.com>
2024-02-18 02:52:01 -0800

    Add LC0200

---
+++ src/leetcode/lc0200.py
@@ -0,0 +1,115 @@
+"""Number of Islands
+
+Link: https://leetcode.com/problems/number-of-islands/
+
+Given an `m x n` 2D binary grid grid which represents a map of `'1'`s (land) and `'0'`s (water), return the number of islands.
+
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+"""
+
+from collections import deque
+from typing import Literal
+
+
+class Solution:
+    directions = {(-1, 0), (0, -1), (1, 0), (0, 1)}  # Up, left, down, right.
+
+    @classmethod
+    def _mark_visited_dfs_recursive(cls, grid, row: int, col: int, islands: int = 0):
+        """Depth-first search method to marks the visited nodes.
+        Visited nodes are marked with `In`, where `n` is the island number.
+        """
+        if (
+            not (0 <= row < len(grid) and 0 <= col < len(grid[0]))
+            or grid[row][col] != "1"
+        ):
+            return
+
+        grid[row][col] = "I" + str(islands)  # Mark visited.
+        for δ_row, δ_col in Solution.directions:
+            Solution._mark_visited_dfs_recursive(
+                grid, row + δ_row, col + δ_col, islands
+            )
+
+        return
+
+    @classmethod
+    def _mark_visited_dfs_iterative(cls, grid, row: int, col: int, islands: int = 0):
+        """Depth-first search method to marks the visited nodes.
+        Visited nodes are marked with `In`, where `n` is the island number.
+        """
+        if (
+            not (0 <= row < len(grid) and 0 <= col < len(grid[0]))
+            or grid[row][col] != "1"
+        ):
+            return
+
+        stack = []
+        stack.append((row, col))
+        grid[row][col] = "I" + str(islands)  # Mark visited.
+
+        while stack:
+            (row, col) = stack.pop()
+            for δ_row, δ_col in Solution.directions:
+                if (
+                    0 <= row + δ_row < len(grid)
+                    and 0 <= col + δ_col < len(grid[0])
+                    and grid[row + δ_row][col + δ_col] == "1"
+                ):
+                    stack.append((row + δ_row, col + δ_col))
+                    grid[row + δ_row][col + δ_col] = "I" + str(islands)  # Visited.
+
+        return
+
+    @classmethod
+    def _mark_visited_bfs(cls, grid, row: int, col: int, islands: int = 0):
+        """Breadth-first search method to marks the visited nodes.
+        Visited nodes are marked with `In`, where `n` is the island number.
+        """
+        if (
+            not (0 <= row < len(grid) and 0 <= col < len(grid[0]))
+            or grid[row][col] != "1"
+        ):
+            return
+
+        queue: deque[tuple[int, int]] = deque()
+        queue.append((row, col))
+        grid[row][col] = "I" + str(islands)  # Visited.
+
+        while queue:
+            (row, col) = queue.popleft()
+            for δ_row, δ_col in Solution.directions:
+                if (
+                    0 <= row + δ_row < len(grid)
+                    and 0 <= col + δ_col < len(grid[0])
+                    and grid[row + δ_row][col + δ_col] == "1"
+                ):
+                    queue.append((row + δ_row, col + δ_col))
+                    grid[row + δ_row][col + δ_col] = "I" + str(islands)  # Visited.
+
+        return
+
+    def numIslands(
+        self, grid: list[list[str]], method: Literal["DFS", "BFS"] = "DFS"
+    ) -> int:
+        if not grid:
+            return 0
+        rows = len(grid)
+        if rows == 0:
+            return 0
+        cols = len(grid[0])
+        if cols == 0:
+            return 0
+
+        strategy = {
+            "DFS": Solution._mark_visited_dfs_iterative,
+            "BFS": Solution._mark_visited_bfs,
+        }
+        islands = 0
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col] == "1":
+                    islands += 1
+                    strategy[method](grid=grid, row=row, col=col, islands=islands)
+
+        return islands
---
+++ tests/tests_leetcode/test_lc0200.py
@@ -0,0 +1,36 @@
+import pytest
+from leetcode import lc0200
+
+examples = [
+    (
+        [
+            ["1", "1", "1", "1", "0"],
+            ["1", "1", "0", "1", "0"],
+            ["1", "1", "0", "0", "0"],
+            ["0", "0", "0", "0", "0"],
+        ],
+        1,
+    ),
+    (
+        [
+            ["1", "1", "0", "0", "0"],
+            ["1", "1", "0", "0", "0"],
+            ["0", "0", "1", "0", "0"],
+            ["0", "0", "0", "1", "1"],
+        ],
+        3,
+    ),
+]
+
+
+@pytest.mark.parametrize("grid, output_true", examples)
+def test_numIslands_dfs(grid: list[list[str]], output_true: int):
+    output_calc = lc0200.Solution().numIslands(grid=grid, method="DFS")
+    assert output_calc == output_true
+
+
+#   TODO: Somehow these tests fail when run via `pdm run`.
+@pytest.mark.parametrize("grid, output_true", examples)
+def test_numIslands_bfs(grid: list[list[str]], output_true: int):
+    output_calc = lc0200.Solution().numIslands(grid=grid, method="BFS")
+    assert output_calc == output_true
