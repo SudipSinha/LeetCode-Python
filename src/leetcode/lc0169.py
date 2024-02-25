567edf652da7308b2d7a88d6e33761c4ed4507f1
Sudip Sinha <sinhsudi@amazon.com>
2024-02-24 17:32:32 -0800

    Add Boyer–Moore majority vote algorithm for LC0169

--- src/leetcode/lc0169.py
+++ src/leetcode/lc0169.py
@@ -11,8 +11,29 @@ from collections import defaultdict
 
 
 class Solution:
-    def majorityElement(self, nums: list[int]) -> int:
+    def majorityElement_hashmap(self, nums: list[int]) -> int | None:
+        """Time complexity: O(len(nums)), space complexity: O(nunique(nums))."""
+        if not nums:
+            return None
         count: defaultdict[int, int] = defaultdict(int)
         for n in nums:
             count[n] += 1
         return max(count, key=count.get)  # type: ignore
+
+    def majorityElement_Boyer_Moore(self, nums: list[int]) -> int | None:
+        """Boyer–Moore majority vote algorithm.
+        Link: https://en.wikipedia.org/wiki/Boyer%E2%80%93Moore_majority_vote_algorithm
+        Time complexity: O(len(nums)), space complexity: O(1).
+        """
+        if not nums:
+            return None
+        count = 0
+        candidate = None
+        for n in nums:
+            if count == 0:
+                candidate = n
+            if n != candidate:
+                count -= 1
+            else:
+                count += 1
+        return candidate
--- tests/tests_leetcode/test_lc0169.py
+++ tests/tests_leetcode/test_lc0169.py
@@ -8,6 +8,12 @@ examples = [
 
 
 @pytest.mark.parametrize("nums, output_true", examples)
-def test_majorityElement(nums: list[int], output_true: int):
-    output_calc = lc0169.Solution().majorityElement(nums=nums)
+def test_majorityElement_hashmap(nums: list[int], output_true: int):
+    output_calc = lc0169.Solution().majorityElement_hashmap(nums=nums)
+    assert output_calc == output_true
+
+
+@pytest.mark.parametrize("nums, output_true", examples)
+def test_majorityElement_Boyer_Moore(nums: list[int], output_true: int):
+    output_calc = lc0169.Solution().majorityElement_Boyer_Moore(nums=nums)
     assert output_calc == output_true
