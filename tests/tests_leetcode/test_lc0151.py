cdf2a38aae61f2c8bc875ad9a60a4a6345a6b2cc
Sudip Sinha <sinhsudi@amazon.com>
2024-03-27 20:58:18 -0700

    Add LC0151

---
+++ tests/tests_leetcode/test_lc0151.py
@@ -0,0 +1,14 @@
+import pytest
+from leetcode import lc0151
+
+examples = [
+    ("the sky is blue", "blue is sky the"),
+    ("  hello world  ", "world hello"),
+    ("a good   example", "example good a"),
+]
+
+
+@pytest.mark.parametrize("s, output_true", examples)
+def test_reverseWords(s: str, output_true: str):
+    output_calc = lc0151.reverseWords(s=s)
+    assert output_calc == output_true
