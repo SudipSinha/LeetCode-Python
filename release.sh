3bd4b66e440f4af774e0ebf140f5326febaecce3
Sudip Sinha <SudipSinha@users.noreply.github.com>
2024-04-08 14:20:56 -0700

    Remove release.sh and add scripts to VSCode tasks

--- release.sh
+++
@@ -1,6 +0,0 @@
-set -e
-pdm run ruff check --select I --fix src tests
-pdm run ruff format src tests
-pdm run mypy src tests
-pdm run coverage run -m pytest && pdm run coverage report --show-missing
-pdm build
