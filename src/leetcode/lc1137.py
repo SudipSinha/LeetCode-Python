a3f0f426be8cf06b3d5095e531fd99df37107170
Sudip Sinha <SudipSinha@users.noreply.github.com>
2024-04-08 23:39:53 -0700

    Add LC1137

---
+++ src/leetcode/lc1137.py
@@ -0,0 +1,44 @@
+"""N-th Tribonacci Number
+
+Link: https://leetcode.com/problems/n-th-tribonacci-number/
+
+The Tribonacci sequence `T_n` is defined as follows:
+
+`T_0 = 0`, `T_1 = 1`, `T_2 = 1`, and `T_{n+3} = T_n + T_{n+1} + T_{n+2}` for `n >= 0`.
+
+Given `n`, return the value of `T_n`.
+"""
+
+
+def tribonacci_hashmap(n: int) -> int:
+    """Time complexity: O(n), space complexity: O(n)."""
+
+    cache = {0: 0, 1: 1, 2: 1}
+
+    def _inner(n: int = n) -> int:
+        if n in cache:
+            return cache[n]
+        cache[n] = _inner(n - 3) + _inner(n - 2) + _inner(n - 1)
+        return cache[n]
+
+    return _inner()
+
+
+def tribonacci_iter(n: int) -> int:
+    """Time complexity: O(n), space complexity: O(1)."""
+
+    if n == 0:
+        return 0
+    elif n in {1, 2}:
+        return 1
+
+    a = 0
+    b = 1
+    c = 1
+    s = 0
+
+    for _ in range(3, n + 1):
+        s = a + b + c
+        (a, b, c) = (b, c, s)
+
+    return s
---
+++ tests/tests_leetcode/test_lc1137.py
@@ -0,0 +1,19 @@
+import pytest
+from leetcode import lc1137
+
+examples = [
+    (4, 4),
+    (25, 1389537),
+]
+
+
+@pytest.mark.parametrize("n, output_true", examples)
+def test_tribonacci_hashmap(n: int, output_true: int):
+    output_calc = lc1137.tribonacci_hashmap(n=n)
+    assert output_calc == output_true
+
+
+@pytest.mark.parametrize("n, output_true", examples)
+def test_tribonacci_iter(n: int, output_true: int):
+    output_calc = lc1137.tribonacci_iter(n=n)
+    assert output_calc == output_true
